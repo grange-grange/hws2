3a3ec26635c6c3a1f58aa44b5b049a254c43c9e5
Alexander <123926431+grange-grange@users.noreply.github.com>
2023-04-06 16:55:11 +0300

    Update GreetingContainer.tsx

--- src/s2-homeworks/hw03/GreetingContainer.tsx
+++ src/s2-homeworks/hw03/GreetingContainer.tsx
@@ -60,7 +60,7 @@ const GreetingContainer: React.FC<GreetingContainerPropsType> = ({
     }
 
     const totalUsers = users.length // need to fix
-    const lastUserName = users[users.length - 1] ?  users[users.length - 1].name : '' // need to fix
+    const lastUserName = users.length > 0 ?  users[users.length - 1].name : '' // need to fix
 
     return (
         <Greeting

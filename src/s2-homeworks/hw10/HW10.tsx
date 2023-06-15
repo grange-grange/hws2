95b4ad3af30bdb704e712633622d1ba7bddcfaaa
Aliaksandr Fisiuk <123926431+grange-grange@users.noreply.github.com>
2023-06-15 10:49:55 +0300

    hw#10

--- src/s2-homeworks/hw10/HW10.tsx
+++ src/s2-homeworks/hw10/HW10.tsx
@@ -14,13 +14,16 @@ import {Loader} from './Loader'
 * */
 
 const HW10 = () => {
-    // useSelector, useDispatch // пишет студент
-    const isLoading = false
 
+    // useSelector, useDispatch // пишет студент
+    const state = useSelector<AppStoreType, { isLoading: boolean }>(state => state.loading)
+    const isLoading = state.isLoading
+    const dispatch = useDispatch()
     const setLoading = () => { // пишет студент // показать крутилку на 1,5 секунд
         // dispatch
-
+        dispatch(loadingAC(true))
         // setTimeout
+        setTimeout(() => dispatch(loadingAC(false)), 1500)
     }
 
     return (
--- src/s2-homeworks/hw10/Loader.tsx
+++ src/s2-homeworks/hw10/Loader.tsx
@@ -1,3 +1,16 @@
 import s from './Loader.module.css'
 
-export const Loader = () => <div className={s.loader}/>
+export const Loader = () => <div className={s.loader}>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+</div>

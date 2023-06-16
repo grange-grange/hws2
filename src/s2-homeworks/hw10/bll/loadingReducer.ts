5bbd5346e709e3a6e2b6227ecef38be35b23fe5a
Aliaksandr Fisiuk <123926431+grange-grange@users.noreply.github.com>
2023-06-16 16:48:43 +0300

    hw#12 theme

--- src/s2-homeworks/hw10/bll/loadingReducer.ts
+++ src/s2-homeworks/hw10/bll/loadingReducer.ts
@@ -12,12 +12,9 @@ export const loadingReducer = (state = initState, action: LoadingActionType): { 
     }
 }
 
-type LoadingActionType = {
-    type: 'CHANGE_LOADING'
-    isLoading: boolean
-}
+type LoadingActionType = ReturnType<typeof loadingAC>
 
-export const loadingAC = (isLoading: boolean): LoadingActionType => ({
+export const loadingAC = (isLoading: boolean) => ({
     type: 'CHANGE_LOADING',
-    isLoading,
-})
+    isLoading
+} as const)
--- src/s2-homeworks/hw12/bll/themeReducer.ts
+++ src/s2-homeworks/hw12/bll/themeReducer.ts
@@ -1,14 +1,18 @@
 const initState = {
     themeId: 1,
 }
+type ChangeThemeIdType = ReturnType<typeof changeThemeId>
 
-export const themeReducer = (state = initState, action: any): any => { // fix any
+export const themeReducer = (state = initState, action: ChangeThemeIdType): {themeId: number} => { // fix any
     switch (action.type) {
         // дописать
-
+        case 'SET_THEME_ID':
+            return {themeId: action.id}
         default:
             return state
     }
 }
 
-export const changeThemeId = (id: number): any => ({ type: 'SET_THEME_ID', id }) // fix any
+export const changeThemeId = (id: number) => {
+    return {type: 'SET_THEME_ID', id} as const
+} // fix any
